3357fd865dbebca29c2119318de711c844d38c0e
Rodrigo Carlos Carvalho Lima Barbosa Leal <rodrigo19962010@live.com>
2017-06-17 10:25:12 -0300

    Create a Read String Value in C

---
+++ ReadValue.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    
+    char fraction1[20];
+    
+    printf("Write the first fraction:\n");
+    scanf("%s", fraction1);
+    
+    printf("Fraction 1 is: %s.\n", fraction1);
+
+    return 0;
+    
+}
+
